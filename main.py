511647fb82dc2c83f2736be4ece061bd152ad15f
Bill Dahm <onkirkwoodhill@sbcglobal.net>
2017-01-31 08:18:12 -0600

    Split form elements by field and retested MainHandler

--- main.py
+++ main.py
@@ -39,31 +39,43 @@ page_header = """
 </html>
 """
 
-main_form = """
-<form action="/main" method="post">
+name_form = """
+<form action="/name" method="post">
     <br>
     <br>
 <!--Field for adding user_name ...-->
     <label>Please enter Username:
         <input type="text" name = "username"/>
     </label>
+<form/>
+"""
 
+pw_form = """
+<form action="/pw" method="post">
+    <br>
+    <br>
 <!-- Field for adding password ...-->
-    <br>
-    <br>
     <label>Please enter Password:
         <input type="password" name = "password"/>
     </label>
     <p><i>Password must be 8 - 20 characters,
         contain only digits and letters and
-        is case-sensitive</i>
+        is case-sensitive.</i>
     </p>
+<form/>
+"""
 
+pw_verify_form = """
+<form action="/pwver" method="post">
 <!-- Field for verifying password ...-->
     <label>Please verify Password:
         <input type="password" name="verify"
     </label>
+<form/>
+"""
 
+email_form = """
+<form action="/email" method="post">
 <!-- Field for entering email address ...-->
     <br>
     <br>
@@ -82,20 +94,29 @@ main_form = """
 
 class MainHandler(webapp2.RequestHandler):
     """ Handles root '/' requests """
+    # The default for self.response.headers is html,
+    # so self.response.headers is not needed ...
+    # self.response.headers['Content-Type'#] = 'text/plain'
+
+    # get in mainhandler just displays the form ...
     def get(self):
-        root_form = main_form
+        username=""
+
+        #% {'username' : }
 
         # if we have an error, make a <p> to display it
         error = self.request.get("error")
         error_element = "<p class='error'>" + error + \
         "</p>" if error else ""
 
-        content = page_header + root_form + \
+        content = page_header + name_form + pw_form + \
+        pw_verify_form + email_form + \
         "in MainHandler" + error_element
         self.response.write(content)
 
-class MainProcess(webapp2.RequestHandler):
-    """ Handles '/main' requests """
+#class MainProcess(webapp2.RequestHandler):
+class NameEnter(webapp2.RequestHandler):
+    """ Handles '/name' requests """
 
     def post(self):
         error_msg = ""
@@ -103,19 +124,24 @@ class MainProcess(webapp2.RequestHandler):
         user_name = self.request.get("username")
 
         # Username cannot be blank ...
-        if user_name == " ":
+        if " " in user_name:
             error_msg = 'User name cannot be blank; please reenter.'
 
         if error_msg != "":
+            error_msg = cgi.escape(error_msg)
             self.redirect('/?error=' + error_msg)
 
-        content = page_header + main_form + \
+        content = page_header + name_form + pw_form + \
+        pw_verify_form + email_form + \
         "<p>In MainProcess</p>" + \
         "<p>username=*" + user_name + "*</p>"
         self.response.write(content)
+        #self.response.write(content)
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.out.write(self.request)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/main', MainProcess)
+    ('/name', NameEnter)
 
 ], debug=True)
