041c708c60fdf054b68d9222e39419e9ffcad501
Bill Dahm <onkirkwoodhill@sbcglobal.net>
2017-01-31 12:28:16 -0600

    Validates username and preserves user's input on form

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ page_header = """
         p {
             margin-left: 200px;
         }
+        div {
+            margin-left: 200px;
+        }
     </style>
 </head>
 <body>
@@ -40,23 +43,24 @@ page_header = """
 """
 
 name_form = """
-<form action="/name" method="post">
+<form method="post">
     <br>
     <br>
 <!--Field for adding user_name ...-->
     <label>Please enter Username:
-        <input type="text" name = "username"/>
+        <input type="text" name = "username" value="%(username)s"/>
     </label>
+    <div style="color:red">%(error)s</div>
 <form/>
 """
 
 pw_form = """
-<form action="/pw" method="post">
+<form method="post">
     <br>
     <br>
 <!-- Field for adding password ...-->
     <label>Please enter Password:
-        <input type="password" name = "password"/>
+        <input type="password" name = "password" value="%(password)s"/>
     </label>
     <p><i>Password must be 8 - 20 characters,
         contain only digits and letters and
@@ -66,21 +70,21 @@ pw_form = """
 """
 
 pw_verify_form = """
-<form action="/pwver" method="post">
+<form method="post">
 <!-- Field for verifying password ...-->
     <label>Please verify Password:
-        <input type="password" name="verify"
+        <input type="password" name="verify" value="%(verify)s"/>
     </label>
 <form/>
 """
 
 email_form = """
-<form action="/email" method="post">
+<form method="post">
 <!-- Field for entering email address ...-->
     <br>
     <br>
     <label>Please enter Email:
-        <input type="text" style="width:50em;" name="email"
+        <input type="text" style="width:50em;" name="email" value="%(email)s"/>
     </label>
     <p>
         <i>Note: Email address is optional !</i>
@@ -92,56 +96,63 @@ email_form = """
 </form>
 """
 
+def ValidName(name):
+    if " " not in name:
+        return name
+
+#def WriteForm(self,error=""):
+#    content=page_header + name_form + pw_form + \
+#    pw_verify_form + email_form
+#    self.response.write(content % {"error":error})
+
 class MainHandler(webapp2.RequestHandler):
     """ Handles root '/' requests """
-    # The default for self.response.headers is html,
+    ""# The default for self.response.headers is html,
     # so self.response.headers is not needed ...
     # self.response.headers['Content-Type'#] = 'text/plain'
 
     # get in mainhandler just displays the form ...
+    def WriteForm(self,error="",username="",password="",verify="",email=""):
+        content=page_header + name_form + pw_form + \
+        pw_verify_form + email_form
+        self.response.out.write(content % {"error":error,
+                                           "username":username,
+                                           "password":password,
+                                           "verify":verify,
+                                           "email":email})
+
     def get(self):
-        username=""
+        #username=""
+        #error=""
 
         #% {'username' : }
 
         # if we have an error, make a <p> to display it
-        error = self.request.get("error")
-        error_element = "<p class='error'>" + error + \
-        "</p>" if error else ""
+        #error = self.request.get("error")
+        #error_element = "<p class='error'>" + error + \
+        #"</p>" if error else ""
 
-        content = page_header + name_form + pw_form + \
-        pw_verify_form + email_form + \
-        "in MainHandler" + error_element
-        self.response.write(content)
-
-#class MainProcess(webapp2.RequestHandler):
-class NameEnter(webapp2.RequestHandler):
-    """ Handles '/name' requests """
+        #content = page_header + name_form + pw_form + \
+        #pw_verify_form + email_form + \
+        #"in MainHandler" + error_element
+        #self.response.write(content)
+        self.WriteForm()
 
     def post(self):
-        error_msg = ""
-        # Check to see if user input username ...
-        user_name = self.request.get("username")
+        # Preserve what the user entered ...
+        user_name = self.request.get('username')
+        user_pw = self.request.get('password')
+        user_verify = self.request.get('verify')
+        user_verify = self.request.get('email')
 
-        # Username cannot be blank ...
-        if " " in user_name:
-            error_msg = 'User name cannot be blank; please reenter.'
+        # Save validated input ...
+        name = ValidName(user_name)
 
-        if error_msg != "":
-            error_msg = cgi.escape(error_msg)
-            self.redirect('/?error=' + error_msg)
-
-        content = page_header + name_form + pw_form + \
-        pw_verify_form + email_form + \
-        "<p>In MainProcess</p>" + \
-        "<p>username=*" + user_name + "*</p>"
-        self.response.write(content)
-        #self.response.write(content)
-        #self.response.headers['Content-Type'] = 'text/plain'
-        #self.response.out.write(self.request)
+        if not name:
+            self.WriteForm("Invalid User Name",user_name)
+        else:
+            self.response.out.write("User Name is Valid!")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/name', NameEnter)
-
+    ('/', MainHandler)
 ], debug=True)
